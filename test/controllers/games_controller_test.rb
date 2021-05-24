f646097df0d23c1a0dbcaeffeba6e221a5f7bb79
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-05-25 00:11:06 +0100

    added the functionalities

---
+++ test/controllers/games_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class GamesControllerTest < ActionDispatch::IntegrationTest
+  test "should get [" do
+    get games_[_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get games_new_url
+    assert_response :success
+  end
+
+  test "should get score" do
+    get games_score_url
+    assert_response :success
+  end
+
+  test "should get ]" do
+    get games_]_url
+    assert_response :success
+  end
+
+end
